e73c728fc3fe166cbe9e53c1c3cc4319d8f05f58
salman-abedin <salman.abedin2@g.bracu.ac.bd>
2020-07-21 03:59:04 +0600

    upload

--- puri.sh
+++ puri.sh
@@ -58,9 +58,9 @@ setfooter() {
 
 setborder() {
     goto 3 0
-    for i in $(seq $COLUMNS); do printf "%s" "-"; done
+    for i in $(seq "$COLUMNS"); do printf "%s" "-"; done
     goto "$((LINES - 2))" 0
-    for i in $(seq $COLUMNS); do printf "%s" "-"; done
+    for i in $(seq "$COLUMNS"); do printf "%s" "-"; done
 }
 
 setheader() {
